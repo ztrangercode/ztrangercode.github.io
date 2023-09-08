4a76665e7420935dddf039ee88c922d00029f365
ztrangercode <29653876+ztrangercode@users.noreply.github.com>
2023-09-08 18:06:15 +0200

    trying to add corner images

--- brollop/src/App.js
+++ brollop/src/App.js
@@ -1,29 +1,36 @@
-import ringar from './ringar.jpg';
-import herrgard from './moholms_herrgard.jpg';
+import { useMediaQuery } from 'react-responsive'
+import down_right_corner from './Down_right_corner.jpg';
+import upper_left_corner from './Upper_left_corner.jpg';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={herrgard} className="App-ringar" alt="ringar" />
-        <p>
-        Datum: 2024-07-20 eller 2024-07-27 (Preliminär) <br/>
-        Tid: 15:00 (Preliminär)</p>
+    <div className="App" style={{ backgroundImage: `url(${upper_left_corner})` }}>
+      <div className="container">
+        <div className="art">
+          <header className="App-header">
+            Sandra och Fredrik
+          </header>
+          <p>
+          Datum: 2024-07-20 eller 2024-07-27 (Preliminär) <br/>
+          Tid: 15:00 (Preliminär)</p>
 
-        <p>Vigsel:<br/>
-        Mo Kyrka<br/>
-        549 92 Moholm</p>
-        <p>Fest: (16:00 till 01:00)<br/>
-        Moholms Herrgård
-        549 92 Moholm</p>
+          <p>Vigsel:<br/>
+          Mo Kyrka<br/>
+          549 92 Moholm</p>
+          <p>Fest: (16:00 till 01:00)<br/>
+          Moholms Herrgård
+          549 92 Moholm</p>
 
-        <p>
-        Övernattning: Det finns några rum på Moholms Herrgård, 
-        se <a href="http://www.moholmsherrgard.se/hotellrum.html">http://www.moholmsherrgard.se/hotellrum.html</a> 
-        och hör av er till oss om ni är intresserade!
-        </p>
-      </header>
+          <p>
+          Övernattning: Det finns några rum på Moholms Herrgård, 
+          se <a href="http://www.moholmsherrgard.se/hotellrum.html">http://www.moholmsherrgard.se/hotellrum.html</a> 
+          och hör av er till oss om ni är intresserade!
+          </p>
+          <img src={down_right_corner} clasName='App-DR' alt="" />
+        </div>
+      </div>
     </div>
   );
 }
