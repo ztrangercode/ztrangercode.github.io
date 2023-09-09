ca49f30388a310d6c21e0031102db7e5ebb97a02
ztrangercode <29653876+ztrangercode@users.noreply.github.com>
2023-09-09 10:32:01 +0200

    Moved images to corner

--- brollop/src/App.js
+++ brollop/src/App.js
@@ -1,36 +1,40 @@
-import { useMediaQuery } from 'react-responsive'
-import down_right_corner from './Down_right_corner.jpg';
-import upper_left_corner from './Upper_left_corner.jpg';
-import './App.css';
-
+//import { useMediaQuery } from 'react-responsive'
+import down_right_corner from "./Down_right_corner.jpg";
+import upper_left_corner from "./Upper_left_corner.jpg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App" style={{ backgroundImage: `url(${upper_left_corner})` }}>
+    <div className="App">
       <div className="container">
         <div className="art">
-          <header className="App-header">
-            Sandra och Fredrik
-          </header>
+          <header className="App-header">Sandra och Fredrik</header>
           <p>
-          Datum: 2024-07-20 eller 2024-07-27 (Preliminär) <br/>
-          Tid: 15:00 (Preliminär)</p>
-
-          <p>Vigsel:<br/>
-          Mo Kyrka<br/>
-          549 92 Moholm</p>
-          <p>Fest: (16:00 till 01:00)<br/>
-          Moholms Herrgård
-          549 92 Moholm</p>
-
-          <p>
-          Övernattning: Det finns några rum på Moholms Herrgård, 
-          se <a href="http://www.moholmsherrgard.se/hotellrum.html">http://www.moholmsherrgard.se/hotellrum.html</a> 
-          och hör av er till oss om ni är intresserade!
+            Datum: 2024-07-20 eller 2024-07-27 (Preliminär) <br />
+            Tid: 15:00 (Preliminär)
           </p>
-          <img src={down_right_corner} clasName='App-DR' alt="" />
+
+          <p>
+            Vigsel: <br />
+            Mo Kyrka <br />
+            549 92 Moholm
+          </p>
+          <p>
+            Fest: (16:00 till 01:00) <br />
+            Moholms Herrgård 549 92 Moholm
+          </p>
+
+          <p>
+            Övernattning: Det finns några rum på Moholms Herrgård, se
+            <a href="http://www.moholmsherrgard.se/hotellrum.html">
+              http://www.moholmsherrgard.se/hotellrum.html
+            </a>
+            och hör av er till oss om ni är intresserade!
+          </p>
         </div>
       </div>
+      <img src={upper_left_corner} className="App-UL" alt="" />
+      <img src={down_right_corner} className="App-DR" alt="" />
     </div>
   );
 }
