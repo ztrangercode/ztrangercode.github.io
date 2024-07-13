cd875184d2f4bdf6bdbbd4128d4a01a7f2c8513d
ztrangercode <29653876+ztrangercode@users.noreply.github.com>
2024-07-13 20:30:55 +0200

    Fixed borders in sudoku solution

--- src/SudokuSolution.tsx
+++ src/SudokuSolution.tsx
@@ -1,10 +1,10 @@
 import React from "react";
+import "./SudokuSolution.css"; // Make sure the path is correct
 
 const SudokuSolution = () => {
   const solution =
     "293861574781435692456972183348296751912357468675148329539724816164589237827613945";
 
-  // Split the solution into rows
   const rows = solution.match(/.{1,9}/g) || []; // Creates an array of rows
 
   return (
@@ -12,18 +12,16 @@ const SudokuSolution = () => {
       style={{ display: "flex", flexDirection: "column", width: "fit-content" }}
     >
       {rows.map((row, rowIndex) => (
-        <div key={rowIndex} style={{ display: "flex" }}>
+        <div
+          key={rowIndex}
+          className={`sudoku-row ${rowIndex === 0 ? "thick-top-border" : ""}`}
+        >
           {row.split("").map((num, colIndex) => (
             <div
               key={colIndex}
-              style={{
-                width: 20,
-                height: 20,
-                border: "1px solid black",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
+              className={`sudoku-cell ${
+                colIndex === 0 ? "thick-left-border" : ""
+              }`}
             >
               {num}
             </div>
